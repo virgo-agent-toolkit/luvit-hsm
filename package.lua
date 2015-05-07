8faeda14980d51b382c5f25d4fffa435d75c72a1
Ryan Phillips <ryan.phillips@rackspace.com>
2015-05-07 13:58:13 -0500

    bump to 1.0.0

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = 'rphillips/hsm',
-  version = '0.0.3',
+  version = '1.0.0',
   description = 'hierarchical state machine',
   author = 'Hiroaki Nakamura <hnakamur@gmail.com>',
   main = 'hsm.lua',
